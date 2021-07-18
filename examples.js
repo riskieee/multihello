d649daaaf63d34aae3c89a3238bc4e8ee629462b
riskieee <git@riskieee.de>
2021-07-18 13:26:56 +0200

    Add test and renamed fix for repo+lib renaming

--- examples.js
+++ examples.js
@@ -1,11 +1,23 @@
-const sayHello = require('./sayHello.js')
+const sayhello = require('./index.js')
 
 console.log('-----------------------------------------------')
-console.log('sayHallo() examples and options')
+console.log('sayhello() examples and options')
 console.log('-----------------------------------------------')
-console.log('sayHello() => random => ', sayHello())
+
 // output > random
-console.log('sayHello(3) => by number => ', sayHello(3))
+console.log('sayhello() => random => ', sayhello())
+
 // output > 'Namaste (Hindi)'
-console.log("sayHello('Kok') => search 'Kok' => ", sayHello('Kok'))
+console.log('sayhello(3) => by number => ', sayhello(3))
+
 // output > 'Deu boro dis dium (Kokani)'
+console.log("sayhello('Kok') => search 'Kok' => ", sayhello('Kok'))
+
+// output > random by emptystring
+console.log("sayhello('') => search '' => ", sayhello(''))
+
+// error output > 'Sorry, no match :('
+console.log(
+	"sayhello('errortest') => search 'errortest' => ",
+	sayhello('errortest')
+)
